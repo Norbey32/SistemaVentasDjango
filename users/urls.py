12ac10bc66e0ba92f1d3bb556b00c627be667500
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-09-21 12:59:18 -0500

    app users creada

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from users.views import RegisterView # Importamos las vistas de autenticación
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+]
---
+++ users/views.py
@@ -0,0 +1,10 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from .forms import RegisterForm
+
+class RegisterView(CreateView):
+    model = User
+    form_class = RegisterForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
